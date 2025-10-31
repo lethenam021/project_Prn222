39bc4834ca1035b52da42b63de38ab9368f31ec4
Le The Nam <namle682004@gmail.com>
2025-11-01 00:12:45 +0700

    update base

---
+++ Project_Prn222/Presentation/Controllers/SellerController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers
+{
+    public class SellerController : Controller
+    {
+        public IActionResult Store()
+        {
+            return View();
+        }
+        public IActionResult ManagerProduct()
+        {
+            return View();
+        }
+        public IActionResult NewProduct()
+        {
+            return View();
+        }
+    }
+}
