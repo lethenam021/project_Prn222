7460ac09703952e30be2359a33bf21e5df89a539
hhh102 <hhhttct102@gmail.com>
2025-11-13 21:15:24 +0700

    Search Inventory

--- Prn222_Project/BusinessLogic/obj/Debug/net9.0/BusinessLogic.AssemblyInfo.cs
+++ Prn222_Project/BusinessLogic/obj/Debug/net9.0/BusinessLogic.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLogic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0914e98805eadec6b8a9b5a18afc3e069e6d83e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47142973c6fe682a3ddd0fd7550d647977682dcf")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLogic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLogic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Prn222_Project/DataAccess/obj/Debug/net9.0/DataAccess.AssemblyInfo.cs
+++ Prn222_Project/DataAccess/obj/Debug/net9.0/DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0914e98805eadec6b8a9b5a18afc3e069e6d83e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47142973c6fe682a3ddd0fd7550d647977682dcf")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
