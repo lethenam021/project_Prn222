b117bb4605c5aa69d0a4804832237ae81d64117e
MinhTri <minhtrik100ilhp@gmail.com>
2025-11-14 15:06:32 +0700

    Init

--- ProjectPRN222/obj/Debug/net9.0/ProjectPRN222.AssemblyInfo.cs
+++ ProjectPRN222/obj/Debug/net9.0/ProjectPRN222.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectPRN222")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd5042c5934aa52332571f5dd4c047bf97c869b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectPRN222")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectPRN222")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
