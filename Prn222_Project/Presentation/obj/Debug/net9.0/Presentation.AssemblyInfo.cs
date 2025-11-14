b643845449045d62c65995f981439fa9f6d0994a
hhh102 <hhhttct102@gmail.com>
2025-11-14 07:26:21 +0700

    Pull dev

--- Prn222_Project/Presentation/obj/Debug/net9.0/Presentation.AssemblyInfo.cs
+++ Prn222_Project/Presentation/obj/Debug/net9.0/Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af6a57aba1d0953251777fadf5fabf31596ab3e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+952219bddbd329283877719a4fc7725fd0ac7133")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
